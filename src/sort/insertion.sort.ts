6872633af309d6376ba57e32d7157a9a6e7887d2
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2026-04-01 23:45:47 +0600

    insertion sort has completed on optimized way

--- src/sort/insertion.sort.ts
+++ src/sort/insertion.sort.ts
@@ -1,5 +1,31 @@
 import array from "../data/array";
 
+// <---- My Initial Way ---->
+// const insertionSort = (arr: number[]) => {
+//   let sortedArr: number[] = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const num = arr[i];
+//     if (sortedArr.length === 0) {
+//       sortedArr.push(num);
+//     } else {
+//       let checkingNumber = sortedArr.length;
+//       for (let x = 0; x < checkingNumber; x++) {
+//         const currentNum = sortedArr[x];
+//         if (num < currentNum) {
+//           sortedArr.splice(x, 0, num);
+//           break;
+//         } else if (x === sortedArr.length - 1) {
+//           sortedArr.push(num);
+//         }
+//       }
+//     }
+//   }
+
+//   return sortedArr;
+// };
+
+// <---- Optimized Way ---->
 const insertionSort = (arr: number[]) => {
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
